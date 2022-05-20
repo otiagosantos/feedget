0c1754e080987553c7244de6a9c9d4ec5cec66ac
Tiago Santos <tiagogs98@outlook.com>
2022-05-20 03:57:27 -0300

    Adiciona cors em aberto.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { routes } from "./routes";
+import cors from 'cors';
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
